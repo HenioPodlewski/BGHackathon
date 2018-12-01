444539f237d6051b18e92accf334b1b8a0634b7c
HenioPodlewski <44540515+HenioPodlewski@users.noreply.github.com>
2018-12-01 11:33:18 -0500

    Update Election.sol

--- Election.sol
+++ Election.sol
@@ -1,101 +1,59 @@
-pragma solidity ^0.4.25;
-//import "./SafeMath.sol";
-contract ElectionFactory {
-    address[] public deployedElections;
-    string name;
-    string electionType;
-    address theElectionOwner = msg.sender;
-
-    function createElection(string memory name, string memory electionType, address theElectionOwner) public {
-       address newElection = new Election(name, electionType, theElectionOwner);
-       deployedElections.push(newElection);
-    }
-
-    function getDeployedElections() public view returns (address[] memory) {
-        return deployedElections;
-    }
-
-}
+pragma solidity ^0.4.24;
 
 contract Election {
     address public owner;
     string public name;
-    string electionType;
-    event ElectionResult(string candidateName, uint voteCount);
+    event Vote(string candidateName, uint voteCount);
 
     struct Candidate {
         string name;
         uint voteCount;
-        string constituency;
-        string affiliation;
-        bool paidup;
-        bool authorized;
     }
 
     struct Voter {
         bool authorized;
         bool voted;
         uint vote;
-        string constituency;
     }
 
+    uint public totalVotes;
     Candidate[] public candidates;
-    //mapping(address => Candidate) public candidates;
     mapping(address => Voter) public voters;
 
-    
-    
-    function Election(string memory _name, string memory _electionType, address campaignOwner) public {
-        owner = campaignOwner;
-        electionType = _electionType;
+    modifier ownerOnly() {
+        require(msg.sender == owner, "Only owner is allowed");
+        _;
+    }
+
+    constructor(string _name) public {
+        owner = msg.sender;
         name = _name;
-
-        // candidates.push(Candidate(_candidate1, 0));
-        // candidates.push(Candidate(_candidate2, 0));
-    }
-    
-    // =================================
-    // Events and modifiers
-    // =================================
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-    modifier notVoted() {
-        require(!voters[msg.sender].voted);
-        _;
-    }
-    modifier voterAuthorized() {
-        require(voters[msg.sender].authorized);
-        _;
     }
 
-    // =================================
-    // Functions
-    // =================================
+    function getNumCandidate() public view returns (uint) {
+        return candidates.length;
+    }
 
-    function authorizeVoter(address _voter, string _constituency) public onlyOwner notVoted {
+    function addCandidate(string _name) ownerOnly public {
+        candidates.push(Candidate(_name, 0));
+    }
+
+    function authorize(address _voter) ownerOnly public {
+        require(!voters[_voter].voted, "Voter already voted");
         voters[_voter].authorized = true;
-        voters[_voter].constituency = _constituency;
     }
-    function unAuthorizeVoter(address _voter) public onlyOwner notVoted voterAuthorized{
-        voters[_voter].authorized = false;
-    }
-    function vote(uint _candidate) public notVoted voterAuthorized  {
-        require(candidates[_candidate].paidup);
-        require(candidates[_candidate].authorized);
+
+    function vote(uint _candidate) public {
+        require(voters[msg.sender].authorized, "Not authorized to vote");
+        require(!voters[msg.sender].voted, "Voter already voted");
+        require(_candidate < candidates.length, "Not a valid candidate");
+
         voters[msg.sender].vote = _candidate;
         voters[msg.sender].voted = true;
 
         candidates[_candidate].voteCount += 1;
+        totalVotes += 1;
+        emit Vote(candidates[_candidate].name, candidates[_candidate].voteCount);
     }
 
-    function end() public {
-        require(msg.sender == owner, "Only owner can end election");
-
-        // Emit event for each candidates results
-        for(uint i=0; i < candidates.length; i++){
-            emit ElectionResult(candidates[i].name, candidates[i].voteCount);
-        }
-    }
 }
